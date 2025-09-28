3e11a3ba8c7e9a329ff508673fe870646b2bee81
Jastinog <jastinog.pro@gmail.com>
2025-09-28 21:29:12 +0300

    Fix login

--- apps/users/views.py
+++ apps/users/views.py
@@ -15,7 +15,7 @@ def login_view(request):
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
-            return redirect("home")
+            return redirect("lecture:home")
         else:
             messages.error(request, "Invalid email or password.")
 
